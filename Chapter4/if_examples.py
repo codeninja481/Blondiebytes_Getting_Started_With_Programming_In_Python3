389d71f7d0803f504259cbe25b3f52eadb20d3fa
John Hughes <hughes.john.jr@gmail.com>
2020-12-29 21:59:24 -0500

    Add files via upload
    Initial commit of if_examples.py, updated commit of relational_operators_example.py

---
+++ Chapter4/if_examples.py
@@ -0,0 +1,6 @@
+x = 10
+y = 15
+z = 20
+
+if y > x and y < z:
+    print("Y is greater than X and Y is less than Z")
---
+++ Chapter4/relational_operators_example.py
@@ -0,0 +1,11 @@
+x = 10
+y = 10
+
+isBigger = x >= y
+isSmaller = x <= y
+isEqual = x == y
+isNotEqual = x != y
+print(isBigger)
+print(isSmaller)
+print(isEqual)
+print(isNotEqual)
