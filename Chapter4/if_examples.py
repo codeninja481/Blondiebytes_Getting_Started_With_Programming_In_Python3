b15e1314ee9e9760046f0574cd184e1f6094b3d0
John Hughes <hughes.john.jr@gmail.com>
2020-12-29 22:03:13 -0500

    Updated files commit
    Committing changes.

--- Chapter4/if_examples.py
+++ Chapter4/if_examples.py
@@ -1,6 +1,8 @@
 x = 10
-y = 15
+y = 10
 z = 20
 
 if y > x and y < z:
-    print("Y is greater than X and Y is less than Z")
+    print("Y is greater than X and Y is less than Z.")
+else:
+    print("Y is not greater than X or Y is not less than Z or both.")
